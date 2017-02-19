7088af48cad5e7a54b44820ac5227d513196a03b
wangq <1281550584@qq.com>
2017-02-19 20:39:16 +0800

    工具类添加

---
+++ ystenbasemoudule/src/main/java/com/qiang/senior/ystenbasemoudule/utils/AppUtils.java
@@ -0,0 +1,15 @@
+package com.qiang.senior.ystenbasemoudule.utils;
+
+/**
+ * Created by wangqiang on 2017/2/19.
+ * App 相关工具类
+ */
+
+public class AppUtils {
+
+    private AppUtils() {
+        throw new UnsupportedOperationException("AppUtils can't instantiate");
+    }
+
+
+}
